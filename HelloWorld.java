b4d1190dd5633790239a61f3d960ca0d699cbbf5
Matt <crazotic18@gmail.com>
2025-08-31 19:35:35 -0700

    Initial commit: HelloWorld program and .gitignore

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
+    /** Adds two numbers. Intentionally simple for debugging demo. */
+    public static int add(int a, int b) {
+        int sum = a + b;  // put a breakpoint here
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int x = 2;
+        int y = 3;
+        int result = add(x, y);
+        System.out.println(x + " + " + y + " = " + result);
     }
 }
